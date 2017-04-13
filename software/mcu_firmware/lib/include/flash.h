3ffc6dbfb96c6134b8e1f0f42679e4384de30bb2
RobisLV <roberts.trops@venta.lv>
2017-04-13 11:58:56 +0300

    Finished the flash opcode and SREG bit tables

--- software/mcu_firmware/lib/include/flash.h
+++ software/mcu_firmware/lib/include/flash.h
@@ -14,42 +14,81 @@
 #include "spi.h"
 
 /******************************************
- *           AT25DF321A OP-CODES
+ *         AT25DF321A OP-CODES
  ******************************************/
-#define FLASH_OPCODE_DATA_WRITE     0x02
-#define FLASH_OPCODE_ENABLE_WRITE   0x06
-#define FLASH_OPCODE_DISABLE_WRITE  0x04
-#define FLASH_OPCODE_STATUS_READ    0x05
+/*  Program (write) and Erase Commands  */
+#define FLASH_OPCODE_ARRAY_READ         0x0B    /* Fclk <= 85MHz            */
+//#define FLASH_OPCODE_ARRAY_READ       0x03    /* Fclk <= 50MHz            */
+//#define FLASH_OPCODE_ARRAY_READ       0x1B    /* Fclk <= 100MHz           */
+#define FLASH_OPCODE_DARRAY_READ        0x3B    /* Dual-Output Read Array   */
+#define FLASH_OPCODE_BLOCK_ERASE_4KB    0x20
+#define FLASH_OPCODE_BLOCK_ERASE_32KB   0x52
+#define FLASH_OPCODE_BLOCK_ERASE_64KB   0xD8
+#define FLASH_OPCODE_CHIP_ERASE         0x60
+//#define FLASH_OPCODE_CHIP_ERASE       0xC7
+#define FLASH_OPCODE_DATA_WRITE         0x02
+#define FLASH_OPCODE_DDATA_WRITE        0xA2
+#define FLASH_OPCODE_WRITE_SUSPEND      0xB0
+#define FLASH_OPCODE_WRITE_RESUME       0xD0
 
-#define FLASH_OPCODE_DATA_READ      0x0B
-#define FLASH_OPCODE_RESET          0xF0
-#define FLASH_OPCODE_ID_READ        0x9F
-#define FLASH_OPCODE_PWR_DOWN       0xB9
-#define FLASH_OPCODE_PWR_UP         0xAB
+/*  Protection commands                 */
+#define FLASH_OPCODE_ENABLE_WRITE       0x06
+#define FLASH_OPCODE_DISABLE_WRITE      0x04
+#define FLASH_OPCODE_SECTOR_PROTECT     0x36
+#define FLASH_OPCODE_SECTOR_UNPROTECT   0x39
+#define FLASH_OPCODE_SECTOR_PROT_READ   0x3C    /* Read Sector Protection Regs     */
+
+/*  Security Commands                   */
+#define FLASH_OPCODE_SECTOR_LOCK        0x33    /* Sector Lockdown                 */
+#define FLASH_OPCODE_FREEZ_LOCK_STATE   0x34    /* Freeze Sector Lockdown State    */
+#define FLASH_OPCODE_SECTOR_LOCK_READ   0x35    /* Read Sector Lockdown Registers  */
+#define FLASH_OPCODE_OTP_WRITE          0x3C    /* Program OTP Security Register   */
+#define FLASH_OPCODE_OTP_READ           0x3C    /* Read OTP Security Registe       */
+
+/*  Status Register Commands            */
+#define FLASH_OPCODE_STATUS_READ        0x05    /* Read Status Register            */
+#define FLASH_OPCODE_STATUS_1_WRITE     0x01    /* Write Status Register Byte 1    */
+#define FLASH_OPCODE_STATUS_2_WRITE     0x31    /* Write Status Register Byte 2    */
+
+/*  Miscellaneous Commands              */
+#define FLASH_OPCODE_RESET              0xF0    /* Chip reset                      */
+#define FLASH_OPCODE_ID_READ            0x9F    /* Read Manufacturer and Device ID */
+#define FLASH_OPCODE_PWR_DOWN           0xB9    /* Deep Power-Down                 */
+#define FLASH_OPCODE_PWR_UP             0xAB    /* Resume from Deep Power-Down     */
 
 /******************************************
  *    AT25DF321A status register bits
  ******************************************/
-#define FLASH_SREG_READY            0x01    /* Ready/Busy Status                    */
-#define FLASH_SREG_WEL              0x02    /* Write Enable Latch Status            */
-#define FLASH_SREG_SWP_L            0x04    /* Software Protection Status           */
-#define FLASH_SREG_SWP_H            0x08    /* Software Protection Status           */
-#define FLASH_SREG_WPP              0x10    /* Write Protect (WP) Pin Status        */
-#define FLASH_SREG_EPE              0x20    /* Erase/Program Error                  */
-#define FLASH_SREG_RES              0x40    /* Reserved for future use              */
-#define FLASH_SREG_SPRL             0x80    /* Sector Protection Registers Locked   */
+/*  Status Register Byte 1              */
+#define FLASH_SREG1_READY               0x01    /* Ready/Busy Status                    */
+#define FLASH_SREG1_WEL                 0x02    /* Write Enable Latch Status            */
+#define FLASH_SREG1_SWP_L               0x04    /* Software Protection Status           */
+#define FLASH_SREG1_SWP_H               0x08    /* Software Protection Status           */
+#define FLASH_SREG1_WPP                 0x10    /* Write Protect (WP) Pin Status        */
+#define FLASH_SREG1_EPE                 0x20    /* Erase/Program Error                  */
+//#define FLASH_SREG1_RESERVED          0x40    /* Reserved for future use              */
+#define FLASH_SREG1_SPRL                0x80    /* Sector Protection Registers Locked   */
+
+/*  Status Register Byte 2              */
+#define FLASH_SREG2_READY               0x01    /* Ready/Busy Status                    */
+#define FLASH_SREG2_ES                  0x02    /* Ready/Busy Status                    */
+#define FLASH_SREG2_PS                  0x04    /* Ready/Busy Status                    */
+#define FLASH_SREG2_SLE                 0x08    /* Ready/Busy Status                    */
+#define FLASH_SREG2_RSTE                0x10    /* Reset enable                         */
+//#define FLASH_SREG2_RESERVED          0x20    /* Reserved for future use              */
+//#define FLASH_SREG2_RESERVED          0x40    /* Reserved for future use              */
+//#define FLASH_SREG2_RESERVED          0x80    /* Reserved for future use              */
 
 /************************************************************
 * SPI flash control functions
 ************************************************************/
-uint16_t flash_data_read    (uint32_t memory_address,uint8_t *storage_buffer, uint16_t storage_buffer_size);
-uint16_t flash_id_read      (uint8_t *storage_buffer, uint16_t storage_buffer_size);
-uint16_t flash_data_write   (uint32_t memory_address,uint8_t *data_buffer, uint16_t data_buffer_size);
+uint16_t flash_data_read        (uint32_t memory_address,uint8_t *storage_buffer, uint16_t storage_buffer_size);
+uint16_t flash_id_read          (uint8_t *storage_buffer, uint16_t storage_buffer_size);
+uint16_t flash_data_write       (uint32_t memory_address,uint8_t *data_buffer, uint16_t data_buffer_size);
 uint16_t flash_status_read      (void);
 uint16_t flash_write_enable     (void);
 uint16_t flash_write_disable    (void);
-uint16_t flash_erase_all();
-uint16_t flash_erase_block();
-
+uint16_t flash_erase_all        (void);
+uint16_t flash_erase_block      (void);
 
 #endif /* LIB_INCLUDE_FLASH_H_ */
